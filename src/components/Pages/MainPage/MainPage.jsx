fa5ee7532fdbbf0e779fc8a94877ed7d4104099a
takaza-codes <elekovalenko@gmail.com>
2025-06-04 21:55:57 +0300

    add basic banner design to mainpage

--- src/components/Pages/MainPage/MainPage.jsx
+++ src/components/Pages/MainPage/MainPage.jsx
@@ -1,5 +1,20 @@
+import banner from "../../../assets/images/banner-image.png";
+import styles from "./MainPage.module.scss";
+
 function MainPage() {
-  return <div>[BANNER]</div>;
+  return (
+    <section className={styles.bannerContainer}>
+      <div>
+        <h1>
+          Investing in <br />
+          Knowledge and <br /> <span>Your Future</span>
+        </h1>
+      </div>
+      <div className={styles.imageContainer}>
+        <img src={banner} alt="Banner" />
+      </div>
+    </section>
+  );
 }
 
 export default MainPage;
