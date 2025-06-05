04f26a3dbdf7288d473cea7a9b1ffe1148cbef84
takaza-codes <elekovalenko@gmail.com>
2025-06-05 22:40:56 +0300

    add error boundary, change main page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import "./styles/fonts.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import ROUTES from "../src/routes/routes";
@@ -16,20 +17,22 @@ function App() {
 
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <main>
-          <Suspense fallback={<div>Wait a bit...</div>}>
-            <Routes>
-              <Route path={ROUTES.MAIN} element={<MainPage />} />
-              <Route path={ROUTES.LIST} element={<WordList />} />
-              <Route path={ROUTES.GAME} element={<WordCards />} />
-              <Route path={ROUTES.ERROR} element={<ErrorPage />} />
-            </Routes>
-          </Suspense>
-        </main>
-        <Footer />
-      </div>
+      <ErrorBoundary>
+        <div className="App">
+          <Header />
+          <main>
+            <Suspense fallback={<div>Wait a bit...</div>}>
+              <Routes>
+                <Route path={ROUTES.MAIN} element={<MainPage />} />
+                <Route path={ROUTES.LIST} element={<WordList />} />
+                <Route path={ROUTES.GAME} element={<WordCards />} />
+                <Route path={ROUTES.ERROR} element={<ErrorPage />} />
+              </Routes>
+            </Suspense>
+          </main>
+          <Footer />
+        </div>
+      </ErrorBoundary>
     </Router>
   );
 }
--- src/components/Pages/MainPage/MainPage.jsx
+++ src/components/Pages/MainPage/MainPage.jsx
@@ -1,19 +1,46 @@
 import banner from "../../../assets/images/banner-image.png";
 import styles from "./MainPage.module.scss";
+import BaseButton from "../../BaseButton/BaseButton";
+import joinIcon from "../../../assets/icons/join-icon.svg";
+import timeIcon from "../../../assets/icons/time-icon.svg";
+import resultIcon from "../../../assets/icons/result-icon.svg";
 
 function MainPage() {
   return (
-    <section className={styles.bannerContainer}>
-      <div>
-        <h1>
-          Investing in <br />
-          Knowledge and <br /> <span>Your Future</span>
-        </h1>
-      </div>
-      <div className={styles.imageContainer}>
-        <img src={banner} alt="Banner" />
-      </div>
-    </section>
+    <>
+      <section className={styles.bannerContainer}>
+        <div className={styles.textContainer}>
+          <h1>
+            Investing in <br />
+            Knowledge and <br /> <span>Your Future</span>
+          </h1>
+          <BaseButton type="save">Get started!</BaseButton>
+        </div>
+        <div className={styles.imageContainer}>
+          <img src={banner} alt="Banner" />
+        </div>
+      </section>
+      <section className={styles.stepsContainer}>
+        <h2>How does it work?</h2>
+        <div className={styles.steps}>
+          <div className={styles.step}>
+            <img src={joinIcon} alt="Create an account icon" />
+            <h3>Create an account</h3>
+            <p>Less than 5 minutes - and you're ready to go</p>
+          </div>
+          <div className={styles.step}>
+            <img src={timeIcon} alt="Invest time icon" />
+            <h3>Invest time every day</h3>
+            <p>As little or as much as you want - regularity is key</p>
+          </div>
+          <div className={styles.step}>
+            <img src={resultIcon} alt="Get results icon" />
+            <h3>Get results</h3>
+            <p>Notice your progress and it'll keep you motivated</p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
