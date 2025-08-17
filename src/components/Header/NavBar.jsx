d57acc3746199b2dbb9cc7f50e521be59581a7d6
takaza-codes <elekovalenko@gmail.com>
2025-08-17 19:21:25 +0300

    add burger menu, add adaptive design to MainPage

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -1,19 +1,40 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import ROUTES from "../../routes/routes";
 import styles from "./NavBar.module.scss";
 
 function NavBar() {
+  const [open, setOpen] = useState(false);
+
+  const handleToggle = () => setOpen((prev) => !prev);
+  const handleClose = () => setOpen(false);
+
   return (
-    <nav>
-      <ul className={styles.navItems}>
+    <nav className={styles.navbar}>
+      <button
+        className={`${styles.hamburger} ${open ? styles.open : ""}`}
+        aria-label="Toggle navigation"
+        aria-expanded={open}
+        onClick={handleToggle}>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+      </button>
+      <ul className={`${styles.navItems} ${open ? styles.open : ""}`}>
         <li className={styles.navItem}>
-          <Link to={ROUTES.MAIN}>Home</Link>
+          <Link to={ROUTES.MAIN} onClick={handleClose}>
+            Home
+          </Link>
         </li>
         <li className={styles.navItem}>
-          <Link to={ROUTES.LIST}>Your dictionary</Link>
+          <Link to={ROUTES.LIST} onClick={handleClose}>
+            Your dictionary
+          </Link>
         </li>
         <li className={styles.navItem}>
-          <Link to={ROUTES.GAME}>Train with cards</Link>
+          <Link to={ROUTES.GAME} onClick={handleClose}>
+            Train with cards
+          </Link>
         </li>
       </ul>
     </nav>
